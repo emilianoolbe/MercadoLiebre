923f8b264517e376f3a315bf4261a0a38cd25b3e
emilianoolbe <emilianoolbe@gmail.com>
2022-08-15 00:58:28 -0300

    estuctura pronta falta todo CSS

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const express = require ("express");
+
 const path = require("path");
 
 let app = express();
+app.use(express.static(path.join(__dirname, "./public")));
+
+app.listen(3010, () => console.log("Servidor Corriendo"));
+
+app.get("/", (req, res) =>{
+    res.sendFile(path.join(__dirname, "./views/html/home.html"));
+})
