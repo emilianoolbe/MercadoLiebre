c45b802e0c5bffd68f99b8fd9a21b1a4291c4895
emilianoolbe <emilianoolbe@gmail.com>
2022-09-07 22:42:24 -0300

    1

--- app.js
+++ app.js
@@ -6,10 +6,6 @@ let app = express();
 
 app.use(express.static(path.join(__dirname, "./public")));
 
-//app.listen(3010, () => console.log("Servidor Corriendo"));
-
-
-
 
 app.get("/", (req, res) =>{
     res.sendFile(path.join(__dirname, "/views/html/home.html"));
