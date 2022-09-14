8bec4838df56ae1885fcf342b0aa3b04b764e8af
emilianoolbe <emilianoolbe@gmail.com>
2022-09-14 19:25:15 -0300

    1

--- src/app.js
+++ src/app.js
@@ -2,19 +2,22 @@
 const express = require ("express");
 const path = require("path");
 
-// Ejecuto express en variable app
-let app = express();
-app.use(express.static(path.join(__dirname, "../public")));
-
 //Importo routes
 const router = require("./routes/homeRouter");
 const routerUser = require('./routes/userRouter'); 
-app.set('views', './views');
+
+// Ejecuto express en variable app 
+let app = express();
+app.use(express.static(path.join(__dirname, "../public")));
+
+//Lineas EJS
+app.set('view engine', 'ejs')
 
 //Detallo rutas
 app.use("/", router);
 app.use("/ingresa", routerUser);
 app.use("/register", routerUser);
+
 
 //Levanto servidor
 app.listen(process.env.PORT || 3010, function() {
