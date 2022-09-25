86e58e1c020a5eada8c7b6256f289c2ecd26ca39
emilianoolbe <emilianoolbe@gmail.com>
2022-09-25 18:40:25 -0300

    multer seteado productos

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -7,6 +7,28 @@ const express = require('express');
 //Ejectuo método router
 const router = express.Router();
 
+//Importo path
+
+const path = require('path');
+
+//Importo Multer
+const multer = require('multer');
+
+//Seteo multer (indico donde guardo file + nombre)
+const storage = multer.diskStorage({
+    destination:(req, file, cb) => {
+        cb(null, path.join(__dirname, '../../public/imagenes'))
+    },
+    filename: (req, file, cb) =>{
+        //creo el nombre de la imagen (concateno el string + fecha en ms + extensión original)
+        let nuevaImg = 'products-' + Date.now() + path.extname(file.originalname);
+        cb(null, nuevaImg)
+    }
+})
+
+//Ejecuto multer con la config anterior para agregarlo a la ruta
+const upload = multer({storage : storage});
+
 //Rutas
 
 //Muestro productos 
@@ -14,7 +36,7 @@ router.get('/ofertas', productsController.ofertas);
 
 //Creación de producto
 router.get('/crear', productsController.crear);
-router.post('/crear', productsController.guardar);
+router.post('/crear', productsController.guardar); // upload.método('name del input')
 
 //Detalle de producto
 router.get('/detalle/:id', productsController.detalle);
