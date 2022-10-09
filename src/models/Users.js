a4c78541aba2c5d10bc2f0537dde7f39c14408a6
emilianoolbe <emilianoolbe@gmail.com>
2022-10-09 02:50:18 -0300

    1

--- src/models/Users.js
+++ src/models/Users.js
@@ -8,53 +8,73 @@ const User = {
     fileName: path.join(__dirname, '../dataBase/users.json'),
 
     //Parseo + lectura de JSON
-    getData: () => { return JSON.parse(fs.readFileSync(this.fileName, 'utf-8')); },
+    getData: function () { return JSON.parse(fs.readFileSync(this.fileName, 'utf-8')); },
 
     //Todos los usuarios
-    findAllUsers: () => { return this.getData(); },
+    findAllUsers: function () { return this.getData(); },
 
     //Busqueda por ID
-    findUserbyPk : (id) => {
+    findUserbyPk : function (id){
         let allUsers = this.findAllUsers();
 
-        let userFound = allUsers.find((cadaElemento) => cadaElemento.id === id);
+        let userFound = allUsers.find((cadaElemento) => cadaElemento.id == id);
 
         return userFound;
      },
 
      //Busqueda por campo (email, pass, usuario...)
-     findUserByFild: (field, text) => {
+     findUserByFild: function (field, text){
         let allUsers = this.findAllUsers();
 
-        let userFound = allUsers.find((cadaElemento) => cadaElemento[field] === text);
+        let userFound = allUsers.find((cadaElemento) => cadaElemento[field] == text);
 
         return userFound;
      },
 
      //Generación de ID
 
-     generateId: () => { 
+     generateId: function () { 
         let allUsers = this.findAllUsers(); 
         let lastUser = allUsers.pop();
-        lastUser ? lastUser.id + 1 : 1;
+        return lastUser ? lastUser.id + 1 : 1;
     },
 
      //Creación usuario
-     create: (userData) => {
+     create: function (userData) {
         let allUsers = this.findAllUsers();
 
         let newUser = {
             id: this.generateId(),
             ...userData
-
         };
+
         allUsers.push(newUser);
 
         fs.writeFileSync(this.fileName, JSON.stringify(allUsers, null , 4));
         return true;
+     },
+
+     //Elimiación de usuario
+     delete: function (id) {
+      let allUsers = this.findAllUsers();
+      let finalUsers = allUsers.filter((cadaElemento) => cadaElemento.id != id);
+      fs.writeFileSync(this.fileName, JSON.stringify(finalUsers, null, 4), 'utf-8');
+      return true;
+     },
+
+     //Edición de usuario
+     update: function(userData){
+      let allUsers = this.findAllUsers();
+      for (cadaElemento of allUsers) {
+         if(this.findUserbyPk()){
+            userData.id = this.generateId();
+         
+         }
+      }
      }
 
 }
 
+console.log(User.findUserByFild('id', 1));
 
-module.exports = Product;
+module.exports = User;
