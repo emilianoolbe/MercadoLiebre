95010dbea908eaf6ca122f9429c9a4f8f03fa483
emilianoolbe <emilianoolbe@gmail.com>
2022-12-09 23:07:39 -0300

    listas validaciones de usuarios

--- public/js/validationForUsers.js
+++ public/js/validationForUsers.js
@@ -17,46 +17,6 @@ let fields = {
     img: false,
     password: false
 };
-//Función donde valido según el campo
-const validateForm = (e) => {
-    //Un switch para según el atributo name: "" se ejectue uno u otro código 
-    switch (e.target.name) {
-        case 'nombre':
-            validateField(expresiones.nombre, e.target, e.target.name, 'El usuario debe ser entre 4 y 16 dígitos, solo puede contener nuúmeros, letras y guion bajo')
-            break;           
-        case 'email':
-            validateField(expresiones.correo, e.target, e.target.name, 'Formato inválido, debe estar compuesto solamente de letras, numeros, puntos, guiones y guion bajo: ejemplo@ejemplo.com');                           
-            break;               
-        case 'fechanacimiento':
-            validateLength(e.target, e.target.name, 'Ingrese una fecha en el campo');
-            break;      
-        case 'domicilio':
-            validateLength(e.target, e.target.name, 'Ingrese una dirección en el campo');
-            break;         
-        case 'img':
-            //e.target.files obtengo un array de los archivos --> [0] porque es 1 solo. 
-            //.name me da un string, lo divido con split donde haya un '.' y el resultado es un array
-            let fileExtension = e.target.files[0].name.split('.').pop().toLowerCase();
-            let extesions =['jpg', 'png', 'gif' ]
-            if (extesions.includes(fileExtension)) {
-                document.getElementById('img').classList.add('inputBorderCorrect');
-                document.getElementById('img').classList.remove('inputBorder');
-                e.target.nextElementSibling.classList.remove('erroresFront');
-                e.target.nextElementSibling.innerText = '';
-                field['img'] = true;
-            } else {
-                document.getElementById('img').classList.remove('inputBorderCorrect');
-                document.getElementById('img').classList.add('inputBorder');
-                e.target.nextElementSibling.classList.add('erroresFront');
-                e.target.nextElementSibling.innerText = 'Debe agregar una imagen, (*) Las extensiones de imagenes permitidas son: .jpg  .png .gif';
-                fields['img'] = false;
-            };    
-            break;
-        case 'password':
-            validateLength(e.target, e.target.name, 'Ingrese una contraseña en el campo');
-            break;
-        };
-};
 //FUNCIONES
 const validateField = (expresiones, input, name, message) => {
     if (expresiones.test(input.value)) {
@@ -86,6 +46,49 @@ const validateLength = (input, name, message) => {
         fields[name] = false;
     };      
 };
+const validateAvatar = (input, id) => {
+    //e.target.files obtengo un array de los archivos --> [0] porque es 1 solo. 
+    //.name me da un string, lo divido con split donde haya un '.' y el resultado es un array
+    let fileExtension = input.files[0].name.split('.').pop().toLowerCase();
+    let extesions =['jpg', 'png', 'gif' ]
+    if (extesions.includes(fileExtension)) {
+        document.getElementById(id).classList.add('inputBorderCorrect');
+        document.getElementById(id).classList.remove('inputBorder');
+        input.nextElementSibling.classList.remove('erroresFront');
+        input.nextElementSibling.innerText = '';
+        fields[id] = true;
+    } else {
+        document.getElementById('img').classList.remove('inputBorderCorrect');
+        document.getElementById('img').classList.add('inputBorder');
+        input.nextElementSibling.classList.add('erroresFront');
+        input.nextElementSibling.innerText = 'Debe agregar una imagen, (*) Las extensiones de imagenes permitidas son: .jpg  .png .gif';
+        fields[id] = false;
+    };    
+};
+//Función donde valido según el campo
+const validateForm = (e) => {
+    //Un switch para según el atributo name: "" se ejectue uno u otro código 
+    switch (e.target.name) {
+        case 'nombre':
+            validateField(expresiones.nombre, e.target, e.target.name, 'El usuario debe ser entre 4 y 16 dígitos, solo puede contener nuúmeros, letras y guion bajo')
+            break;           
+        case 'email':
+            validateField(expresiones.correo, e.target, e.target.name, 'Formato inválido, debe estar compuesto solamente de letras, numeros, puntos, guiones y guion bajo: ejemplo@ejemplo.com');                           
+            break;               
+        case 'fechanacimiento':
+            validateLength(e.target, e.target.name, 'Ingrese una fecha en el campo');
+            break;      
+        case 'domicilio':
+            validateLength(e.target, e.target.name, 'Ingrese una dirección en el campo');
+            break;         
+        case 'img':
+            validateAvatar(e.target, e.target.name)
+            break;
+        case 'password':
+            validateLength(e.target, e.target.name, 'Ingrese una contraseña en el campo');
+            break;
+        };
+};
 //Itero sobre el array y sobre cada input ejecuto un evento
 inputs.forEach((input) =>{
     input.addEventListener('keyup', validateForm);
@@ -95,6 +98,7 @@ inputs.forEach((input) =>{
 
 formulario.addEventListener("submit", (e) =>{
     e.preventDefault();
+    console.log(fields);
     if (fields.nombre && fields.password && fields.img && fields.fechanacimiento && fields.email && fields.domicilio) {
         formulario.submit();
     }else{
