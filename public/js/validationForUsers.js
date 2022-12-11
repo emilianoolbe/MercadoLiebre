00751ec390690e4e61aa10b86581511b73c9b5ce
emilianoolbe <emilianoolbe@gmail.com>
2022-12-11 11:19:01 -0300

    validaciones Login prontas

--- public/js/validationForUsers.js
+++ public/js/validationForUsers.js
@@ -7,6 +7,7 @@ const inputs = document.querySelectorAll('#formulario input');
 const expresiones = {
     nombre: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras y espacios, pueden llevar acentos.
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    password: /^.{4,12}$/, // 4 a 12 digitos.
 };
 //Creo un objeto literal, donde al momento en donde no haya error cambio su valor a true
 let fields = {
@@ -17,52 +18,49 @@ let fields = {
     img: false,
     password: false
 };
-//FUNCIONES
-const validateField = (expresiones, input, name, message) => {
-    if (expresiones.test(input.value)) {
+let validetedErrors = (input, name, message, isError = true ) => {
+    if (isError) {
+        document.getElementById(name).classList.remove('is-invalid');
+        document.getElementById(name).classList.remove('inputBorder');
+        document.getElementById(name).classList.add('is-valid');
         document.getElementById(name).classList.add('inputBorderCorrect');
         input.nextElementSibling.classList.remove('erroresFront');
         input.nextElementSibling.innerText = ''
-        fields[name] = true;
+        fields[name] = true;     
     } else {
+        document.getElementById(name).classList.remove('is-valid');
         document.getElementById(name).classList.remove('inputBorderCorrect');
+        document.getElementById(name).classList.add('is-invalid');  
         document.getElementById(name).classList.add('inputBorder');
         input.nextElementSibling.classList.add('erroresFront');
         input.nextElementSibling.innerText = message
         fields[name] = false;
+    };
+};
+//FUNCIONES
+const validateField = (expresiones, input, name, message) => {
+    if (expresiones.test(input.value) && input.length !== 0) {
+       validetedErrors(input, name, message, true);
+    } else {
+        validetedErrors(input, name, message, false)
     };       
 };
 const validateLength = (input, name, message) => {
     if (input.value.trim().length != 0) {
-        document.getElementById(name).classList.add('inputBorderCorrect');
-        input.nextElementSibling.classList.remove('erroresFront');
-        input.nextElementSibling.innerText = '';
-        fields[name] = true;
+        validetedErrors(input, name, message, true);
     } else {
-        document.getElementById(name).classList.remove('inputBorderCorrect');
-        document.getElementById(name).classList.add('inputBorder');
-        input.nextElementSibling.classList.add('erroresFront');
-        input.nextElementSibling.innerText = message;
-        fields[name] = false;
+        validetedErrors(input, name, message, false);
     };      
 };
-const validateAvatar = (input, id) => {
+const validateAvatar = (input, name) => {
     //e.target.files obtengo un array de los archivos --> [0] porque es 1 solo. 
     //.name me da un string, lo divido con split donde haya un '.' y el resultado es un array
     let fileExtension = input.files[0].name.split('.').pop().toLowerCase();
-    let extesions =['jpg', 'png', 'gif' ]
+    let extesions =['jpg', 'png', 'gif' ];
     if (extesions.includes(fileExtension)) {
-        document.getElementById(id).classList.add('inputBorderCorrect');
-        document.getElementById(id).classList.remove('inputBorder');
-        input.nextElementSibling.classList.remove('erroresFront');
-        input.nextElementSibling.innerText = '';
-        fields[id] = true;
+        validetedErrors(input, name, true);
     } else {
-        document.getElementById('img').classList.remove('inputBorderCorrect');
-        document.getElementById('img').classList.add('inputBorder');
-        input.nextElementSibling.classList.add('erroresFront');
-        input.nextElementSibling.innerText = 'Debe agregar una imagen, (*) Las extensiones de imagenes permitidas son: .jpg  .png .gif';
-        fields[id] = false;
+        validetedErrors(input, name, 'Debe agregar una imagen, (*) Las extensiones de imagenes permitidas son: .jpg  .png .gif', false);
     };    
 };
 //Función donde valido según el campo
@@ -70,7 +68,7 @@ const validateForm = (e) => {
     //Un switch para según el atributo name: "" se ejectue uno u otro código 
     switch (e.target.name) {
         case 'nombre':
-            validateField(expresiones.nombre, e.target, e.target.name, 'El usuario debe ser entre 4 y 16 dígitos, solo puede contener nuúmeros, letras y guion bajo')
+            validateField(expresiones.nombre, e.target, e.target.name, 'El usuario debe ser entre 4 y 16 dígitos, solo puede contener números, letras y guion bajo');
             break;           
         case 'email':
             validateField(expresiones.correo, e.target, e.target.name, 'Formato inválido, debe estar compuesto solamente de letras, numeros, puntos, guiones y guion bajo: ejemplo@ejemplo.com');                           
@@ -85,7 +83,7 @@ const validateForm = (e) => {
             validateAvatar(e.target, e.target.name)
             break;
         case 'password':
-            validateLength(e.target, e.target.name, 'Ingrese una contraseña en el campo');
+            validateField(expresiones.password, e.target, e.target.name, 'La contraseña tiene que tener un largo entre 4 y 12 dígitos');
             break;
         };
 };
